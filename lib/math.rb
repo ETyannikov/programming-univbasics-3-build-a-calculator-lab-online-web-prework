899af400ec14b7a7ae9f78cf98420d46a772dcda
Eddy Tyannikov <etyannikov@gmail.com>
2019-08-30 16:58:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-  num1 + num2
+  num3 = num1 + num2
+  num3
 end
 
 def subtraction(num1, num2)
-
+  num3 = num1 - num2
+  num3
 end
 
 def division(num1, num2)
-
+  num3 = num1 / num2
+  num3
 end
 
 def multiplication(num1, num2)
-
+  num3 = num1 * num2
+  num3
 end
 
 def modulo(num1, num2)
-
+  num3 = num1 % num2
+  num3
 end
 
 def square_root(num)
-
+  num3 = num.square_root
+  num3
 end
